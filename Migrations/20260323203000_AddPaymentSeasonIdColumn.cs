4953c57243ae29aa59fd6f71ee41fd2fbd361840
shaunn-holden <shaunm78@me.com>
2026-03-23 16:35:11 -0400

    Fix payment season migration metadata

--- Migrations/20260323203000_AddPaymentSeasonIdColumn.cs
+++ Migrations/20260323203000_AddPaymentSeasonIdColumn.cs
@@ -1,9 +1,13 @@
+using GymBudgetApp;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace GymBudgetApp.Migrations
 {
+    [DbContext(typeof(AppDbContext))]
+    [Migration("20260323203000_AddPaymentSeasonIdColumn")]
     public partial class AddPaymentSeasonIdColumn : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
